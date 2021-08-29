6b4635b81475ce255876bcc00c5f5cfc557debfe
Dubstabber <taesan@protonmail.com>
2021-08-29 16:36:58 +0200

    Update packages

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-          preload: path.join(__dirname, 'preload.js')
-      }
-    })
-  
-    win.loadFile('index.html')
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    darkTheme: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-    createWindow()
-  })
+  createWindow();
+});
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
