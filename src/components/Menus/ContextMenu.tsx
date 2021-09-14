4790c76904acae05238ddcd3b143286bc9d3ec9c
Dubstabber <taesan@protonmail.com>
2021-09-14 15:03:42 +0200

    Add submenu for context menu and submenu classes and icons

--- src/components/Menus/ContextMenu.tsx
+++ src/components/Menus/ContextMenu.tsx
@@ -1,6 +1,7 @@
 import { MouseEvent } from 'react';
 
 import './ContextMenu.css';
+import '../../css/host-station-icons.css';
 
 const ContextMenu = ({ posX, posY }: any) => {
   function handleClick(e: MouseEvent) {
@@ -8,21 +9,51 @@ const ContextMenu = ({ posX, posY }: any) => {
     e.stopPropagation();
   }
 
+  function addHostStation(id: number): void {
+    console.log(`create a host station: ${id}`);
+  }
+
   return (
     <ul
       onClick={handleClick}
       onContextMenu={handleClick}
-      className="menu contextMenu"
+      className="contextMenu"
       style={{ top: posY, left: posX }}
     >
-      <li className="menu__item">Add host station here</li>
-      <li>Add squad here</li>
-      <li>Set sector faction to</li>
-      <li>Set height here</li>
-      <li>Add special building here</li>
-      <li>Set building here</li>
-      <li>Add sector item here</li>
-      <li>Clear this sector</li>
+      <li className="menu__items">
+        Add host station here
+        <ul className="menu__submenu">
+          <li className="menu__item" onClick={() => addHostStation(1)}>
+            <i className="host-station-icon resistance-icon"></i>Resistance
+          </li>
+          <li className="menu__item" onClick={() => addHostStation(6)}>
+            <i className="host-station-icon ghorkov-icon"></i>Ghorkov
+          </li>
+          <li className="menu__item" onClick={() => addHostStation(4)}>
+            <i className="host-station-icon taerkasten-icon"></i>Taerkasten
+          </li>
+          <li className="menu__item" onClick={() => addHostStation(3)}>
+            <i className="host-station-icon mykonian-icon"></i>Mykonian
+          </li>
+          <li className="menu__item" onClick={() => addHostStation(2)}>
+            <i className="host-station-icon sulgogar-icon"></i>Sulgogar
+          </li>
+          <li className="menu__item" onClick={() => addHostStation(5)}>
+            <i className="host-station-icon black-sect-icon"></i>Black Sect
+          </li>
+          <li className="menu__item" onClick={() => addHostStation(7)}>
+            <i className="host-station-icon training-icon"></i>Training host
+            station
+          </li>
+        </ul>
+      </li>
+      <li className="menu__items">Add squad here</li>
+      <li className="menu__item">Set sector faction to</li>
+      <li className="menu__item">Set height here</li>
+      <li className="menu__items">Add special building here</li>
+      <li className="menu__item">Set building here</li>
+      <li className="menu__items">Add sector item here</li>
+      <li className="menu__item">Clear this sector</li>
     </ul>
   );
 };
--- src/components/Menus/ViewMenu.tsx
+++ src/components/Menus/ViewMenu.tsx
@@ -3,26 +3,58 @@ import { toggleMenu, hoverMenu } from '../../actions/menuActions';
 import { StoreState } from '../../reducers';
 import { MenuProps } from './Menus';
 
-const _ViewMenu = ({id, menu:{activatedMenu, hoverMenuId}, toggleMenu, hoverMenu}:MenuProps) => {
-    return (
-        <>
-            <div className="menu__container">
-                <span onClick={toggleMenu} onMouseOver={() => hoverMenu(id)} className="menu__title">View</span>
-                <ul className={activatedMenu && id === hoverMenuId ? "menu__list--enabled" : "menu__list--disabled"} id="viewMenu">
-                    <li className="menu__item" id="showLevelEditor">Show level editor</li>
-                    <li className="menu__item" id="toggleSectorImages">Toggle sector images</li>
-                    <li className="menu__item" id="toggleHeightValues">Toggle height values</li>
-                    <li className="menu__item" id="toggleSectorValues">Toggle sector values</li>
-                    <li className="menu__item" id="toggleOwnerValues">Toggle owner values</li>
-                    <li className="menu__item" id="toggleBuildingsValues">Toggle buildings values</li>
-                </ul>
-            </div>
-        </>
-    )
-}
+const _ViewMenu = ({
+  id,
+  menu: { activatedMenu, hoverMenuId },
+  toggleMenu,
+  hoverMenu,
+}: any) => {
+  return (
+    <>
+      <div className="menu__container">
+        <span
+          onClick={toggleMenu}
+          onMouseOver={() => hoverMenu(id)}
+          className="menu__title"
+        >
+          View
+        </span>
+        <ul
+          className={
+            activatedMenu && id === hoverMenuId
+              ? 'menu__list--enabled'
+              : 'menu__list--disabled'
+          }
+          id="viewMenu"
+        >
+          <li className="menu__item" id="showLevelEditor">
+            Show level editor
+          </li>
+          <li className="menu__item" id="toggleSectorImages">
+            Toggle sector images
+          </li>
+          <li className="menu__item" id="toggleHeightValues">
+            Toggle height values
+          </li>
+          <li className="menu__item" id="toggleSectorValues">
+            Toggle sector values
+          </li>
+          <li className="menu__item" id="toggleOwnerValues">
+            Toggle owner values
+          </li>
+          <li className="menu__item" id="toggleBuildingsValues">
+            Toggle buildings values
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 const mapStateToProps = (state: StoreState) => ({
-    menu: state.menu
-})
+  menu: state.menu,
+});
 
-export const ViewMenu = connect(mapStateToProps, {toggleMenu, hoverMenu})(_ViewMenu);
+export const ViewMenu = connect(mapStateToProps, { toggleMenu, hoverMenu })(
+  _ViewMenu
+);
